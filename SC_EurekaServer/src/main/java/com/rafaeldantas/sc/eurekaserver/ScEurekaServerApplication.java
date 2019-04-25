8642f043afa1670f9bed7f81ce221daf3098c5ae
Rafael Dantas <dantasrafael@yahoo.com.br>
2019-04-24 22:52:37 -0300

    first commit

---
+++ SC_EurekaServer/src/main/java/com/rafaeldantas/sc/eurekaserver/ScEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.rafaeldantas.sc.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ScEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScEurekaServerApplication.class, args);
+	}
+
+}
